a056ddaf81e8e5627e84b33f6763c1313af80106
Karan Rana <karanrana1706@gmail.com>
2025-04-25 23:34:33 +0530

    flutter-timer initial commit

--- flutter_counter/lib/counter_observer.dart
+++ flutter_counter/lib/counter_observer.dart
@@ -6,6 +6,6 @@ class CounterObserver extends BlocObserver {
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    //print('${bloc.runtimeType} $change');
+    print('${bloc.runtimeType} $change');
   }
 }
